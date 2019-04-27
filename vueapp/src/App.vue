4da3ddc85a1f90c9a9481bbadabdebbe1ebf6e58
p992202933 <992202933@qq.com>
2019-04-27 10:28:50 +0800

    vueapp

---
+++ vueapp/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+  <common-header></common-header>
+  </div>
+</template>
+<script>
+import CommonHeader from '@/components/CommonHeader.vue'
+export default {
+  
+}
+</script>
+<style lang='scss' scoped>
+
+</style>
---
+++ vueapp/src/components/CommonHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <span>首页</span>
+        <span>电影</span>
+    </div>
+</template>
+<script>
+    
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
