2732291969a12f8231c2f3c084fb7bf60e0e6389
p992202933 <992202933@qq.com>
2019-05-08 15:04:39 +0800

    app

--- vueapp/src/views/movie.vue
+++ vueapp/src/views/movie.vue
@@ -1,15 +1,62 @@
 <template>
-    <div>
-电影
+    <div class="movie-container">
+        <ul>
+            <li class="movie-list" v-for="(obj, index) in movieList" :key="index">
+                <img class="movie-img" :src="obj.images.medium">
+                <div class="movie-text">
+                    <h4>{{ obj.title }}</h4>
+                    <p>
+                        <span v-for="(cast,index) in obj.casts" :key="index">{{cast.name}}</span>
+                    </p>
+                    <p>
+                        已观看：66666
+                    </p>
+                    <p>
+                        年份：2019
+                    </p>
+                    <p>
+                        类型：奇幻
+                    </p>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
-        
+        data() {
+            return {
+                movieList:[]
+            }
+        },
+        created() {
+           axios.get('/data/movie0.json')
+            .then((result) => {
+                this.movieList = result.data.subjects;
+                console.log(this.movieList);
+            }
+            )},
     }
 </script>
 
 <style scoped>
-
+ .movie-container{
+     padding: 0 0.2rem ;
+ }
+ .movie-img{
+     width: 100px;
+     height: 150px;
+     margin-right: 0.2rem;
+ }
+ .movie-list{
+     color: black;
+     display: flex;
+     border-bottom: 1px black solid;
+     padding: 0.2rem 0;
+ }
+ .movie-text{
+     flex: 1;
+ }
 </style>
