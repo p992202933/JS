c6aa462d07b399d2aa67b7db2e08ead49a06ad66
p992202933 <992202933@qq.com>
2019-05-08 16:42:08 +0800

    app

--- vueapp/src/views/movie.vue
+++ vueapp/src/views/movie.vue
@@ -32,18 +32,36 @@
         data() {
             return {
                 movieList:[],
-                isShow: true
+                isShow: true,
+                isBottom: false
             }
         },
         created() {
-           axios.get('https://bird.ioliu.cn/v1?url=https://api.douban.com/v2/movie/in_theaters?city=广州&start=0&count=10')
+            this.getMovie(),
+            window.onscroll=()=>{
+                let scrollTop = document.documentElement.scrollTop;
+                let height = document.documentElement.scrollHeight;
+                let clinetHeight = document.documentElement.clientHeight;
+                if( scrollTop+clinetHeight==height && !isBottom){
+                    this.getMovie();
+                }
+            }
+           },
+           methods: {
+               getMovie(){
+               axios.get('https://bird.ioliu.cn/v1?url=https://api.douban.com/v2/movie/in_theaters?city=广州&start='+this.getMovie.length+'&count=10')
            //axios.get('/data/movie0.json')
             .then((result) => {
-                
-                this.movieList = result.data.subjects;
+                this.isShow = false;
+                this.movieList = [...this.movieList,...result.data.subjects];
                 console.log(this.movieList);
-            }
-            )},
+                if(this.movieList.length == result.data.subjects.total){
+                    this.isBottom = true;
+                }
+            })
+
+               }
+           },
     }
 </script>
 
